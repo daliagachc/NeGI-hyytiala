e8e528b71d72d426e7e1d672fab6b1738ad1ff65
daliagachc <daliaga@chacaltaya.edu.bo>
2017-10-24 13:23:51 +0300

    Create instance_lainch_pip.sh

---
+++ instance_lainch_pip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Script that downloads
+## some source data and iPython notebooks
+## into a Jupyter image for Python and Machine Learning course
+
+cd /home/jovyan/work
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+git clone https://github.com/daliagachc/NeGI-hyytiala.git
+
+pip install bokeh
+pip install basemap
