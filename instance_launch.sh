39ae853d39854e228e5248fef1f61178dc4ed74f
daliagachc <daliaga@chacaltaya.edu.bo>
2017-10-25 00:07:31 +0300

    Update instance_launch.sh

--- instance_launch.sh
+++ instance_launch.sh
@@ -8,9 +8,15 @@ cd /home/jovyan/work
 # even if you're only cloning
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
+# diego's repo
 git clone https://github.com/daliagachc/NeGI-hyytiala.git
+#richards' repo
+git clone https://github.com/richard-olav/ebas-data-retrieval-session.git
+#bokeh's
+git clone https://github.com/bokeh/bokeh-notebooks.git
 
-mkdir dummy
-
+pip install --upgrade pip
+pip install netCDF4
+pip install pydap
 conda install bokeh
 conda install basemap
